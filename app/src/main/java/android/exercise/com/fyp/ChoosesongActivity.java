b2bcde122b69142c5aff45f7f9a2aefef9d13eb6
hinson23701 <104848707+hinson23701@users.noreply.github.com>
2022-12-06 01:55:35 +0800

    Initial commit

--- app/src/main/java/android/exercise/com/fyp/ChoosesongActivity.java
+++ app/src/main/java/android/exercise/com/fyp/ChoosesongActivity.java
@@ -1,6 +1,8 @@
 package android.exercise.com.fyp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,6 +10,12 @@ public class ChoosesongActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choosesong);
+
+    }
+    public void onClickplay(View view) {
+        Intent intent = new Intent(this, PianoActivity.class );
+        startActivity(intent);
+
 
     }
 }
