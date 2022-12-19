6868f9b136b25150d5f79c7dc86a06622581ef95
hinson23701 <104848707+hinson23701@users.noreply.github.com>
2022-12-19 19:21:02 +0800

    Initial commit

--- app/src/main/java/android/exercise/com/fyp/ChoosesongActivity.java
+++ app/src/main/java/android/exercise/com/fyp/ChoosesongActivity.java
@@ -1,5 +1,7 @@
 package android.exercise.com.fyp;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -17,8 +19,35 @@ public class ChoosesongActivity extends AppCompatActivity {
 
     }
     public void onClickplayabc(View view) {
-        Intent intent = new Intent(this, PianoActivity.class );
-        startActivity(intent);
+        AlertDialog.Builder builder = new AlertDialog.Builder(ChoosesongActivity.this);
+
+        // Set the message show for the Alert time
+        builder.setMessage("Welcome to ABC song practice/games,Do you want play now?");
+
+        // Set Alert Title
+        builder.setTitle("ABC Song playing");
+
+        // Set Cancelable false for when the user clicks on the outside the Dialog Box then it will remain show
+        builder.setCancelable(false);
+
+        // Set the positive button with yes name Lambda OnClickListener method is use of DialogInterface interface.
+        builder.setPositiveButton("Play!", (DialogInterface.OnClickListener) (dialog, which) -> {
+            // When the user click yes button then app will close
+            Intent intent = new Intent(this, PianoActivity.class );
+            startActivity(intent);
+        });
+
+        // Set the Negative button with No name Lambda OnClickListener method is use of DialogInterface interface.
+        builder.setNegativeButton("No", (DialogInterface.OnClickListener) (dialog, which) -> {
+            // If user click no then dialog box is canceled.
+            dialog.cancel();
+        });
+
+        // Create the Alert dialog
+        AlertDialog alertDialog = builder.create();
+        // Show the Alert Dialog box
+        alertDialog.show();
+
 
 
     }
